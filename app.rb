aa36d6b9f29f02de206fdabbbc2abb71c5941d1a
Tomas Perez <tomasitopg6@gmail.com>
2020-08-07 04:39:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,36 +2,43 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name' do
+  get "/reversename/:name" do
     @name = params[:name]
-    erb :reversename
+    "#{@name.reverse}"
   end
 
-  get '/square/:number' do
-    @num = params[:number].to_i
-    erb :square
+  get "/square/:number" do
+    (params[:number].to_i ** 2).to_s
   end
 
-  get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    erb :say
+  get "/say/:number/:phrase" do
+    list_phrase = ' '
+
+    params[:number].to_i.times do
+      list_phrase += params[:phrase]
+    end
+    list_phrase
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    erb :saywords
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-    erb :operation
+  get "/:operation/:number1/:number2" do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    if params[:operation] == "add"
+      (num1 + num2).to_s
+    elsif params[:operation] == "subtract"
+      (num1 - num2).to_s
+    elsif params[:operation] == "multiply"
+      (num1 * num2).to_s
+    elsif params[:operation] == "divide"
+      (num1 / num2).to_s
+    else
+      "Unable to perform this operation"
+    end
   end
 
 end
